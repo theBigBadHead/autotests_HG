2b7fa5263a0c40bcbfd59f63e56a41257f22cab8
radmir <r2dm1r@gmail.com>
2025-08-19 10:21:58 +0300

    change tittle

--- tests/test_category.py
+++ tests/test_category.py
@@ -10,7 +10,7 @@ from tools.routes import AppRoute
 @pytest.mark.regression
 @pytest.mark.ui
 class TestCategories:
-    @allure.title("Проверка категории 'Промо-одежда' и открытие первой ссылки из категории")
+    @allure.title("Проверка наличия всех категорий и коллекций")
     def test_check_category_and_collections(self, main_page: MainPage):
         main_page.visit(AppRoute.ROOT)
         main_page.header_categories_component.open()
